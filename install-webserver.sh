b471b01e28c98e8d9563ce2c0b9ecbab6cfd18ca
Nandini <nandini9590@gmail.com>
2015-11-21 12:42:17 -0600

    adding the code from mp2

---
+++ install-webserver.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sudo apt-get update && sudo apt-get install -y apache2 git php5 php5-mysql php5-curl mysql-client curl
+cd /var/www/html
+sudo git clone https://github.com/Nandini-ITM544/MPFinal_Application_Setup
+sudo mv -v MPFinal_Application_Setup/* /var/www/html
+sudo rm -rf /var/www/html/MPFinal_Application_Setup
+cd /home/controller
+sudo curl -sS https://getcomposer.org/installer | php
+sudo php composer.phar require aws/aws-sdk-php
+sudo mv vendor /var/www/html 
+PW=letmein
+export PW
